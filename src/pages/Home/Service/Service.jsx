6a87e5d3c6f0360285e30cf9d04a48f0d79027bf
Al Hasanul Banna <tutul5800@gmail.com>
2023-06-20 00:40:31 +0600

    service area

--- src/pages/Home/Service/Service.jsx
+++ src/pages/Home/Service/Service.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Service.css";
+import {
+  AiOutlineGlobal,
+  AiOutlineShoppingCart,
+  AiOutlineCode,
+  AiOutlineApi,
+  AiOutlineLayout,
+  AiOutlineTool,
+} from "react-icons/ai";
+import { BsArrowRightCircle } from "react-icons/bs";
 
 const Service = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setServices(data);
+      });
+  }, []);
+
+  const getIconComponent = (iconName) => {
+    switch (iconName) {
+      case "AiOutlineGlobal":
+        return <AiOutlineGlobal size={32} />;
+      case "AiOutlineShoppingCart":
+        return <AiOutlineShoppingCart size={32} />;
+      case "AiOutlineCode":
+        return <AiOutlineCode size={32} />;
+      case "AiOutlineApi":
+        return <AiOutlineApi size={32} />;
+      case "AiOutlineLayout":
+        return <AiOutlineLayout size={32} />;
+      case "AiOutlineTool":
+        return <AiOutlineTool size={32} />;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div>
-      <h2>This is service section</h2>
+    <div className="service-area">
+      <div className="container">
+        <div className="section-title">
+          <h2>My Services</h2>
+        </div>
+        <div className="all-services">
+          {services.map((service) => (
+            <div className="single-service" key={service.id}>
+              {getIconComponent(service.icon)}
+              <h3>{service.title}</h3>
+              <p>{service.description}</p>
+              <span className="right-arrow">
+                <BsArrowRightCircle></BsArrowRightCircle>
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
