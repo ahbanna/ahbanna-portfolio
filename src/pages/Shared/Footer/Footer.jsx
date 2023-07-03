ac6053587444a5ee7ea207f014db395478ae64cf
Al Hasanul Banna <tutul5800@gmail.com>
2023-07-03 23:31:06 +0600

    footer added

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="copyright">
+        <p>© 2023 Al Hasanul Banna. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
